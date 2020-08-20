01a96eca81f9a993092071a03d3990530cf3b1d1
Grace Bai <63276360+potatolight@users.noreply.github.com>
2020-08-20 13:30:48 -0700

    add new properties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -149,9 +149,14 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const query =  `INSERT INTO properties(owner_id, title, description,thumbnail_photo_url,cover_photo_url,cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country,street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`;
+  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms,property.number_of_bedrooms, property.country, property.street,property.city,property.province,property.post_code];
+  const promise = client.query(query, values)
+  return promise.then(res => {
+    // console.log(res.rows[0])
+    return res.rows[0];
+  })
 }
 exports.addProperty = addProperty;
